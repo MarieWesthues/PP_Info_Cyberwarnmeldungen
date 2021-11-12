8f386f0198922fde5ab39a3ae98a8c77502721fd
giggity-hub <imigstefan@gmail.com>
2021-11-12 22:44:11 +0100

    initialize sveltekit with windicss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+<a href=".">Home</a>
+ <a href="about">About</a>
+<a href="settings">Settings</a>
+</nav>
+    
+<slot></slot>
+<style windi:preflights:global windi:safelist:global>
+</style>
