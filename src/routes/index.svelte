c419058aa81cf6b82940880175520a98d2f2fcd1
giggity-hub <imigstefan@gmail.com>
2021-11-19 19:05:30 +0100

    added link to routes/examples

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,5 @@
 
 <h1 class="text-blue-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/examples" class="text-red-600 text-6xl font-bold hover:text-red-400"> Go to examples</a>
