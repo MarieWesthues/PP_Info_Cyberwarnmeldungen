9a50ede193c4437614f2ed3a2edf7ee378f3361e
giggity-hub <imigstefan@gmail.com>
2021-11-20 01:08:03 +0100

    added minimal example api

---
+++ src/lib/api/index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const uri = `mongodb+srv://${process.env['USER']}:${process.env['PASSWORD']}@cluster0.d0wqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, (err) => {
+    if (err) {
+        throw err
+    }else{
+        console.log("🎉 connected to DB");
+    }
+})
+
+// Create DB Schema
+const kittySchema = new mongoose.Schema({
+    name: String
+  });
+
+export const Kitten = mongoose.model('Kitten', kittySchema); 
+
