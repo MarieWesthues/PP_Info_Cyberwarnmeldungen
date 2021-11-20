1d5cb2396113275f068cc5738b2f193b339b233a
giggity-hub <imigstefan@gmail.com>
2021-11-20 01:08:35 +0100

    mongoose api example frontend

---
+++ src/routes/kittens/index.json.js
@@ -0,0 +1,19 @@
+import {Kitten} from '$lib/api';
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function post(request) {
+       
+        const cat = new Kitten({
+            name: request.body.name
+        })
+
+        const res = await cat.save()
+
+        console.log(res);
+
+		return {
+			body: {
+				cat
+			}
+		};
+}
---
+++ src/routes/kittens/index.svelte
@@ -0,0 +1,16 @@
+<script>
+    import axios from 'axios';
+
+    let inputVal="";
+
+    const clickHandler = () => {
+        axios.post('/kittens.json', {
+            name: inputVal,
+        }).then(console.log)
+    }
+</script>
+
+<h1 class="font-bold text-xl text-center m-5">Welcome to the kittens input</h1>
+
+Name:<input bind:value={inputVal} class="border border-dark-400 rounded-xl" type="text">
+<button  on:click={clickHandler} class="bg-blue-400">Submit</button>
