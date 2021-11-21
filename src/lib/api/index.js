abe2bd8ef96442185fd3fe90272d36894978beab
giggity-hub <imigstefan@gmail.com>
2021-11-21 11:11:16 +0100

    added configuration Schema

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -14,10 +14,44 @@ mongoose.connect(uri, (err) => {
     }
 })
 
+
+const threatTypeSchema = new Schema({
+    name: String,
+    description: String,
+})
+
+const threatLevelSchema = new Schema({
+    name: String,
+    color: String,
+})
+
+const messageAttributeSchema = new Schema({
+    key: String,
+    type: String,
+    value: Schema.Types.Mixed,
+})
+
+const configurationSchema = new Schema({
+    name: String,
+    replyEmail: String,
+    logo: Buffer, //Buffer is used for Binary data like blob
+    colors: {
+        primary: String,
+        secondary: String,
+    },
+    threatTypes: [threatTypeSchema],
+    threatLevels: [threatLevelSchema],
+    messageAttributes: [messageAttributeSchema], 
+
+})
+
+
+
+
 // Create DB Schema
 const kittySchema = new mongoose.Schema({
     name: String
-  });
+});
 
 export const Kitten = mongoose.model('Kitten', kittySchema); 
 
