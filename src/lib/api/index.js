f427ae6686aff05e62d320188f66c9c7a23717c6
giggity-hub <imigstefan@gmail.com>
2021-11-21 17:46:38 +0100

    created mongoose Schema from DB Diagram

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -14,6 +14,8 @@ mongoose.connect(uri, (err) => {
     }
 })
 
+// Configuration Schema
+const {Mixed, ObjectId} = Schema.Types;
 
 const threatTypeSchema = new Schema({
     name: String,
@@ -28,7 +30,8 @@ const threatLevelSchema = new Schema({
 const messageAttributeSchema = new Schema({
     key: String,
     type: String,
-    value: Schema.Types.Mixed,
+    value: Mixed,
+    usedBy: [ObjectId]
 })
 
 const configurationSchema = new Schema({
@@ -42,11 +45,112 @@ const configurationSchema = new Schema({
     threatTypes: [threatTypeSchema],
     threatLevels: [threatLevelSchema],
     messageAttributes: [messageAttributeSchema], 
-
 })
 
+// Templates
 
+const templateSchema = new Schema({
+    cert_id: ObjectId,
+    channel: String,
+    name: String,
+    used: Boolean,
+    templateString: String,
+    usesAttributes: [ObjectId],
+    matches: {
+        type: String,
+        level: String,
+    },
+})
 
+// Subscriber & groups
+const subscriberSchema = new Schema({
+    cert_id: ObjectId,
+    name: String,
+    email: String,
+    intern: Boolean,
+    groups: [ObjectId],
+})
+
+const groupSchema = new Schema({
+    cert_id: ObjectId,
+    name: String,
+    description: String,
+    color: String,
+    subscribers: [ObjectId],
+})
+
+// User
+const userPermissions = new Schema({
+    sendAlert: Boolean,
+    createAlert: Boolean,
+    editSubscribers: Boolean,
+    editGroups: Boolean,
+    editConfig: Boolean,
+})
+
+const userSchema = new Schema({
+    cert_id: ObjectId,
+    email: String,
+    name: String,
+    password: String,
+    permissions: userPermissions,
+})
+
+// Alert and history
+const alertAttributeSchema = new Schema({
+    key: String,
+    value: Mixed,
+})
+
+const pendingAlertSchema = new Schema({
+    cert_id: ObjectId,
+    dateCreated: Date,
+    createdBy: ObjectId,
+    threatLevel: String,
+    threatType: String,
+    title: String,
+    intern: Boolean,
+    selectedGroups: [ObjectId],
+    selectedChannels: [String],
+    attributes: [alertAttributeSchema]
+})
+
+const alertLinkSchema = new Schema({
+    uri: String,
+    channel: ObjectId,
+})
+
+const persistedAlertSchema = new Schema({
+    cert_id: ObjectId,
+    dateCreated: Date,
+    createdBy: ObjectId,
+    threatLevel: String,
+    threatType: String,
+    title: String,
+    intern: Boolean,
+    selectedGroups: [ObjectId],
+    attributes: [alertAttributeSchema],
+    dateSend: Date,
+    authorizedBy: ObjectId,
+    links: [alertLinkSchema]
+})
+
+const channelSchema = new Schema({
+    name: String,
+    colors: {
+        primary: String,
+        onPrimary: String,
+    },
+    logo: String, //link to logo 
+    messageFormat: String,
+    authenticated: Boolean,
+})
+
+// Cert
+const certSchema = new Schema({
+    configuration: configurationSchema,
+    channels: [channelSchema],
+})
 
 // Create DB Schema
 const kittySchema = new mongoose.Schema({
