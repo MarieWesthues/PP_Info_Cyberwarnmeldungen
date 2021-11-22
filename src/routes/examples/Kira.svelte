576a8adde1f4c3f9cf781244e6f950584496b8e4
Kira257 <kira.szalai@web.de>
2021-11-22 16:33:54 +0100

    Create Kira.svelte
    Erstellen der Testseite Kira.svelte

---
+++ src/routes/examples/Kira.svelte
@@ -0,0 +1,31 @@
+<script>
+    let myName = "Kira"
+    let myAge = "23"
+    let myHobbies = ["Reisen","Lesen","Fitness"]
+</script>
+
+<main class="container mx-auto bg-blue-200 flex flex-col items-center">
+    <h1 class="heading">
+        
+    </h1>
+    
+    <p>
+        Mein Name ist {myName} und ich bin {myAge} Jahre alt.
+    </p>
+    
+    <h1 class="heading">
+       
+    </h1>
+    <ul class="bg-green-500 w-1/2">
+        {#each hobbies as hobby, i }
+            <li>{i+1}) {hobby}</li>
+        {/each}
+    </ul>
+</main>
+
+
+<style>
+    .heading{
+        @apply text-xl font-bold text-green-300
+    }
+</style>
