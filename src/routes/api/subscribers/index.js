05ff914e09620fb51aeacf281e58956b21531ad6
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-11-23 20:42:21 +0100

    api endpoint for subscriber example

--- src/routes/api/subscribers/index.js
+++ src/routes/api/subscribers/index.js
@@ -19,29 +19,13 @@ export async function post(request) {
 // GET api/subscriber (eine Liste von allen subscribern erhalten)
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ params }) {
-    
+    console.log("bruh wtf");
     const subscribers = await Subscriber.find({})
 
      return {
-         body: {
-             subscribers
-         }
+         body: subscribers
      };
 }
 
-// POST api/subscriber (neuen Subscriber erstellen)
-/** @type {import('@sveltejs/kit').RequestHandler} */
-export async function put(request) {
 
-    const res = await Subscriber.findByIdAndUpdate(request.params._id, request.body, function(err, doc) {
-        if (err) return res.send(500, {error: err});
-        return res.send('Succesfully saved.');
-    });
-
-     return {
-         body: {
-             res
-         }
-     };
-}
 
