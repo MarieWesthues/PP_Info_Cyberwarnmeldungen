f9500c616c024f961ca59f3b858c5a2c2eb7361b
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-11-23 16:20:40 +0100

    import custom bootstrap

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,16 @@
 <script>
     import Nav from "$lib/Nav.svelte";
+    // import "smelte/src/tailwind.css"
+    import './global_styles.scss';
 
 </script>
 
 <Nav/>
 
 <slot></slot>
-<style windi:preflights:global windi:safelist:global>
+<style>
 
 </style>
+<!-- <style windi:preflights:global windi:safelist:global>
+
+</style> -->
