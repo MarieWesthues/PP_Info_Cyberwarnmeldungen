99ec30fc761f7b68268aa341a76272783f43103f
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-11-26 14:04:56 +0100

    created templates route

---
+++ src/routes/api/templates/index.js
@@ -0,0 +1,14 @@
+import Template from '$lib/mongoose/model/template/template';
+
+// POST api/subscriber (neuen Subscriber erstellen)
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function post(request) {
+    const subscriber = new Template(request.body)
+     const res = await subscriber.save()
+
+     return {
+         body: {
+             res
+         }
+     };
+}
