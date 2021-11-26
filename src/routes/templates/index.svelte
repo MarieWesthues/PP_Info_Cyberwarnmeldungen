b9a86787e37f1909f02ba748b90b3814b7a30379
JSimmou <jamal.simmou@stud.uni-due.de>
2021-11-26 17:53:31 +0100

    Template Entwurf
    - Array angelegt
    - Komponenten erstellt
    - Button erstellt
    - Grundgerüst
    - es fehlen: Preview, Liste, Statekrams

---
+++ src/routes/templates/index.svelte
@@ -0,0 +1,39 @@
+<script>
+
+let array = [
+    {cert_id: "randomString1", 
+    channel: "twitter", 
+    name: "Warnmeldung1",
+    used: true,
+    templatesString: "test",
+    usesAttributes: [],
+    matches: {type: "maleware", level: "2"}},
+
+    {cert_id: "randomString2", 
+    channel: "reddit", 
+    name: "Warnmeldung2",
+    used: false,
+    templatesString: "test",
+    usesAttributes: [],
+    matches: {type: "scam", level: "1"}},
+    
+    {cert_id: "randomString3", 
+    channel: "e-mail", 
+    name: "Warnmeldung3",
+    used: true,
+    templatesString: "test",
+    usesAttributes: [],
+    matches: {type: "virus", level: "4"}},
+    
+    {cert_id: "randomString4", 
+    channel: "discord", 
+    name: "Warnmeldung4",
+    used: true,
+    templatesString: "test",
+    usesAttributes: [],
+    matches: {type: "virus", level: "4"}}
+    ];
+
+
+
+</script>
