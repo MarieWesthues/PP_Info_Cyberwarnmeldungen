097bce8631eab6a193550f84ad35000ffd4a9de3
Kira257 <kira.szalai@web.de>
2021-11-27 11:04:55 +0100

    Create Nav copy.svelte
    Erstellung neuer Branch, um nav bar anzulegen

---
+++ src/lib/Nav copy.svelte
@@ -0,0 +1,44 @@
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+  
+      <div class="navbar-header">
+        ...
+      </div>
+  
+      <div class="collapse navbar-collapse" id="navbar">
+        ...
+      </div>
+  
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            
+            <li><a href="#">Send</a></li>
+            <li><a href="#">Templates</a></li>
+            <li><a href="#">Channels</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#">Subscribers</a></li>
+            <li><a href="#">Statistics</a></li>
+            <li><a href="#">Config</a></li>
+        
+            <li><a href="#">Menüpunkt</a></li> #muss noch bearbeitet werden
+            <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                
+                <ul class="dropdown-menu"> <a href="#">Subscribers<span class="sr-only">(current)</span></a></li> # muss noch bearbeitet werden
+                    <ul class="Subscribers">
+                        <li><a href="#">Groups</a></li>
+                        <li><a href="#">Subscribers</a></li>
+                    </ul>
+                </ul>
+        </ul>
+     
+      </div><!-- /.navbar-collapse -->
+  </nav>
+
+<style>
+    .nav__item{
+        @apply text-white min-w-32 ;
+    }
+</style>
