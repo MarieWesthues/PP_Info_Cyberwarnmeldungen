8b648e3129389fa5bdfd12a74e62ad30e7714572
giggity-hub <imigstefan@gmail.com>
2021-11-28 09:17:49 +0100

    laptop running out of battery

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+
+    export let path;
+    console.log(path);
+    
+
+    import {
+      Collapse,
+      Navbar,
+      NavbarToggler,
+      NavbarBrand,
+      Nav,
+      NavItem,
+      NavLink,
+      Dropdown,
+      DropdownToggle,
+      DropdownMenu,
+      DropdownItem
+    } from 'sveltestrap';
+  
+    let isOpen = false;
+  
+    function handleUpdate(event) {
+      isOpen = event.detail.isOpen;
+    }
+
+    const navItems = [
+        {href: "/send", label: 'Send'},
+        {href: "/subscribers", label: "Subscribers"},
+        {href: "/groups", label: "Groups"},
+        {href: "templates", label: "Templates"}
+        ]
+</script>
+  
+  <Navbar color="light" light expand="md">
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+      <Nav class="me-auto" navbar>
+
+        {#key path}
+        {#each navItems as {href, label}, i }
+        <NavItem>
+            <NavLink {href}>{label}</NavLink>
+        </NavItem>
+    {/each}
+    soos {path}
+        {/key}
+
+      </Nav>
+    </Collapse>
+  </Navbar>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,21 @@
+<script context="module">
+    let path;
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ page, fetch, session, stuff }) {
+        path = page.path;
+
+        return {}
+	}
+</script>
+
 <script>
-    import Nav from "$lib/Nav.svelte";
+    import Nav from "$lib/components/Nav.svelte";
     // import "smelte/src/tailwind.css"
     import './global_styles.scss';
 
 </script>
 
-<Nav/>
+<Nav {path}/>
 
 <slot></slot>
 <style>
