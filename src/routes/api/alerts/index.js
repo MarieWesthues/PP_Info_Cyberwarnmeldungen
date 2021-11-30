e346fbf34122b48bcb221586c8b52cf834707579
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-11-30 12:50:49 +0100

    use certId from store in request

--- src/routes/api/alerts/index.js
+++ src/routes/api/alerts/index.js
@@ -1,8 +1,13 @@
 import {PendingAlert} from '$lib/mongoose/model/alert';
+import { certIdStore } from '$lib/stores';
+import {get as getStoreValue} from 'svelte/store'
 
 // POST api/alerts (neue Warnmeldung erstellen)
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function post(request) {
+export async function post(request) { 
+
+    
+
     const pendingAlert = new PendingAlert(request.body)
 
      const res = await pendingAlert.save()
@@ -18,7 +23,7 @@ export async function post(request) {
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ params }) {
     console.log("Errorrr");
-    const pendingAlerts = await PendingAlert.find({})
+    const pendingAlerts = await PendingAlert.find({cert_id: getStoreValue(certIdStore)})
 
      return {
          body: pendingAlerts
