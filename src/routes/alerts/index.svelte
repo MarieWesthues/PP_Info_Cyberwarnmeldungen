d346010d3f22a849bf057d016b5505cf6a2ec02d
Kira257 <kira.szalai@web.de>
2021-11-30 10:50:23 +0100

    SendingAlerts Seite erstellt & Alerts Code eingefügt

--- src/routes/alerts/index.svelte
+++ src/routes/alerts/index.svelte
@@ -3,7 +3,7 @@
 if a new alert is to be created go to alerts/new
 
 -->
-<script>
+<!--<script>
     let objs = [{id: 'soos'}, {id: 'saaas'}]
 </script>
 
@@ -13,3 +13,74 @@ if a new alert is to be created go to alerts/new
         <a href="alerts/{obj.id}">click</a>
     </div>
 {/each}
+
+
+  import SendingAlerts from "./SendingAlerts.svelte";
+
+   
+        function addMessage () {
+
+    }
+
+-->
+
+<script>
+import { page } from "$app/stores";
+
+
+    
+let message = "Message Title";
+
+</script>
+
+
+
+<html lang="en">
+    <body>
+        
+
+        {#each message as obj}
+            <!--erste Bootstrap Komponente wuw-->
+            <div class="rounded"> Message Title
+                <button type="button" id="button" class="btn btn-primary pull-right"  on:click={()=> setSelection(obj)}> Review </button>
+            </div>
+        {/each}
+    
+        
+        <button type="button" id="Hinzufügen" class="btn btn-primary pull-right"  on:click= {() => addMessage()}> + </button> 
+        
+    </body>
+</html>
+
+
+<style>
+    .rounded
+    {
+        background-color: gray;
+        color: white;
+        padding: 10px;
+        display: flex;
+        align-items: center;
+        margin: 5%;
+        height: 100px;
+        width: 1200px;
+    }
+
+    #button{
+        align-content: right;
+        margin-left: 85%;
+    }
+
+    #Hinzufügen{
+        margin-left: 92%;
+        margin-bottom: 50px;
+        background-color: white;
+        border-radius: 50%;
+        
+    }
+
+
+</style>
+
+
+
