e15893c2815a1e889e9df53d1f358d8ec07d95d7
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-11-30 12:50:17 +0100

    example of load function

--- src/routes/alerts/index.svelte
+++ src/routes/alerts/index.svelte
@@ -1,15 +1,38 @@
-<!-- index for alert overview.
-[slug] is for editing alerts
-if a new alert is to be created go to alerts/new
+<!-- Module Scripts werden auf dem Server ausgeführt -->
+<script context="module">
 
--->
-<script>
+
+// API requests sollten in einem module ausgeführt werden, damit die Notwendigen Daten schon bei Page load zur verfügung stehen
+
+// Für mehr info über die load function siehe => https://kit.svelte.dev/docs#loading
+export async function load({fetch}){
+    const url = '/api/alerts';
+    const res = await fetch(url)
+    const alerts = await res.json()
+
+    // Inhalt von props wird Komponenten als properties zur verfügung gestellt
+    if (res.ok){
+        return {
+            props: {
+                alerts
+            }
+        }
+    }
+}
+</script>
+
+
+<!-- Use lang="ts" for typescript support -->
+<script lang="ts">
+    // In dem alerst property kann man nun auf die API response zugreifen
+    export let alerts;
     let objs = [{id: 'soos'}, {id: 'saaas'}]
 </script>
 
-{#each objs as obj }
+{#each alerts as alert }
     <div>
-        {obj.id}
-        <a href="alerts/{obj.id}">click</a>
+        {alert.title}
+        <a href="alerts/{alert._id}">click</a>
     </div>
 {/each}
+
