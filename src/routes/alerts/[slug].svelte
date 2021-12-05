1f2b0e372de809de7aee973e2fd11956c578d9ba
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-05 14:50:22 +0100

    use input factory

---
+++ src/routes/alerts/[slug].svelte
@@ -0,0 +1,10 @@
+<script>
+import InputFactory from "./InputFactory.svelte";
+
+    const alertAttributes = [{key: "remote", type: "BOOLEAN", value: false}]
+</script>
+
+
+{#each alertAttributes as attribute}
+    <InputFactory attribute={attribute}/>
+{/each}
