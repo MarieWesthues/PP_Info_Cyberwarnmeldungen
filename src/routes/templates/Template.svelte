8e02a6061f93fb47108bcdb37e1366cc083bc712
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-06 16:45:53 +0100

    kleinen hint geadded

--- src/routes/templates/Template.svelte
+++ src/routes/templates/Template.svelte
@@ -2,7 +2,7 @@
     import { object_without_properties } from "svelte/internal";
     import selectedObject from "../index.svelte";
 
-
+    // es muss das on click event weiter geleitet werden. See svelte event propagation oder bubbeling kp wie das heisst
     export let obj;
 </script>
 
