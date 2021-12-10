10795c4c3b60e9e66a7026229ea88da03b1793bc
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-10 14:43:05 +0100

    add userId Store

--- src/lib/stores/index.js
+++ src/lib/stores/index.js
@@ -1,3 +1,5 @@
 import {readable} from 'svelte/store';
 
-export const certIdStore = readable('61acd1207e097f738c2681f0')
+export const certIdStore = readable('61acd1207e097f738c2681f0');
+
+export const userIdStore = readable('61b34c31277d9c606b6ab48e');
