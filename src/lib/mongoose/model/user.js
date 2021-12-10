84376e05d6d72d8e74609985e1e4638f0cda669c
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-10 14:42:52 +0100

    update user schema

--- src/lib/mongoose/model/user.js
+++ src/lib/mongoose/model/user.js
@@ -3,17 +3,17 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 const {Mixed, ObjectId} = Schema.Types;
 
-export const userPermissionsSchema = new Schema({
+export const userPermissionsSchema = {
     sendAlert: {type: Boolean, required: true},
     createAlert: {type: Boolean, required: true},
     editSubscribers: {type: Boolean, required: true},
     editGroups: {type: Boolean, required: true},
     editConfig: {type: Boolean, required: true},
-})
-export const userPermissions = mongoose.model('UserPermissions', userPermissionsSchema);
+}
+// export const userPermissions = mongoose.model('UserPermissions', userPermissionsSchema);
 
 export const userSchema = new Schema({
-    cert:{
+    cert_id:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Cert',
         required: true
