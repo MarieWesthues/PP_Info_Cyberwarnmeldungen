0309e975fabb1185ccfe33b386315630293a9cf8
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-11 16:04:25 +0100

    added new button component

---
+++ src/lib/components/NewButton.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let href;
+
+
+</script>
+
+<div class="wrapper">
+    <a {href}>
+        <button type="button" class="btn btn-primary btn-lg">
+            +
+        </button>
+    </a>
+</div>
+
+
+<style>
+    .wrapper{
+        position: fixed;
+        z-index: 1000;
+        bottom: 3rem;
+        right: 3rem;
+    }
+</style>
