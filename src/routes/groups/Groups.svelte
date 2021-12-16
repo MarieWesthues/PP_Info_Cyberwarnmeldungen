50cf19b809a25822c32697e19eb65725417b62b2
domzydz <zydziok@gmail.com>
2021-12-16 16:03:53 +0100

    Anzahl der Member anzeigen in Groups

--- src/routes/groups/Groups.svelte
+++ src/routes/groups/Groups.svelte
@@ -1,16 +1,13 @@
 <script>
     export let group;
+    export let anzahl = group.subscribers.length;
 </script>
 
 
 <div class="card" style="margin: 25px auto;">
     <h5 class="card-header fs-4">{group.name}</h5>
       <div class="card-body">
-        <!-- {#each group as group }
-            <p class="card-text fw-light fs-6"><span class="border border-2 rounded-pill">Example Member</span></p>
-        {/each} -->
-        
-        
+        <p class="card-text fw-light fs-6"  style="width:5000px;"><span class="border border-2 rounded-pill">  Members = {anzahl}  </span></p>
         <div class="buttons text-end">
           <a href="/groups/{group._id}">
               <button class="btn btn-outline-primary border-2">Edit</button>
