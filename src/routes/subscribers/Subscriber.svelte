e7acdc9d387a1c70990f34be2ea3250065504845
Lisa <lisa.langen@stud.uni-due.de>
2021-12-16 16:59:14 +0100

    subscribers styling/intern/gruppen

--- src/routes/subscribers/Subscriber.svelte
+++ src/routes/subscribers/Subscriber.svelte
@@ -1,30 +1,33 @@
 <script>
+
     export let subscriber;
-    // Es muss noch angezeigt werden ob der subscriber intern ist oder nicht
+
 </script>
 
 
 <div class="card">
-    <h5 class="card-header fs-4">{subscriber.name}</h5>
-      <div class="card-body">
-        <p class="card-title fw-light fs-5">{subscriber.email}</p>
-        {#each subscriber.groups as group }
-            <!-- Hier kann man den tatsächlichen group name noch nicht anzeigen weil die groups noch nicht populatet sind -->
-            <p class="card-text fw-light fs-6"><span class="border border-2 rounded-pill">Example Group Name</span></p>
-        {/each}
+    <h5 class="card-header fs-4">{subscriber.name}
+        {#if subscriber.intern == true}
+            <span class="intern border border-2 rounded-pill fw-light fs-6">intern</span>
+        {/if}
+    </h5>
+        <div class="card-body">
+            <p class="card-title fw-light fs-5">{subscriber.email}</p>
+                {#each subscriber.groups as group }
+                    <span class="card-text fw-light fs-6"><span class="border border-2 rounded-pill list-group-horizontal" style="background:{group.color}">{group}</span></span>
+                {/each}
         
-        
-        <div class="buttons text-end">
-          <a href="/subscribers/{subscriber._id}">
-              <button class="btn btn-outline-primary border-2">Edit</button>
-          </a>
+            <div class="buttons text-end">
+                <a href="/subscribers/{subscriber._id}">
+                    <button class="btn btn-outline-primary border-2">Edit</button>
+                </a>
         
           <!-- um es simpler zu halten lassen wir den delete button erstmal nur auf der subscribers/[id] seite -->
           <!-- <button class="btn btn-outline-danger border-2" on:click={deleteMe}>
               Delete
           </button> -->
-        </div>
-  </div> 
+            </div>
+        </div> 
 </div>
 
 <style>
@@ -32,8 +35,22 @@
     .card {
         width: 45%;
         margin: 0 auto;
+        overflow: auto;
+        margin-bottom: 0.5em;
+    }
+    .border {
+        padding-left: 0.4em;
+        padding-right: 0.4em;
+        padding-top: 0.1;
+        padding-bottom: 0.13em;
+        display:inline-block;
+        margin-bottom: 0.2em;
+        margin-inline-end: 0.2em;
+    }
+    .intern {
+        padding-left: 0.4em;
+        padding-right: 0.4em;
+        background-color:#ffaf0496;
     }
     
-    
-    </style>
-
+</style>
