de18213606e5a19133139ba5e04d7c32e4758377
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-27 16:31:29 +0100

    use bootstrap instead of sveltestrap

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,5 @@
 <script>
-    
+    import {page} from "$app/stores"
 
     import {
       Collapse,
@@ -19,27 +19,31 @@ import CustomNavLink from './CustomNavLink.svelte';
     }
 
     const navItems = [
-        {href: "/alerts", label: 'Send'},
-        {href: "/subscribers", label: "Subscribers"},
-        {href: "/groups", label: "Groups"},
-        {href: "/templates", label: "Templates"}
+        {href: "/alerts", label: 'Send', faClass: 'fas fa-search'},
+        {href: "/subscribers", label: "Subscribers", faClass: 'fas fa-search'},
+        {href: "/groups", label: "Groups", faClass: 'fas fa-search'},
+        {href: "/templates", label: "Templates", faClass: 'fas fa-search'}
         ]
 </script>
   
-  <Navbar color="white" light expand="sm" class="fixed-top shadow-sm ">
-    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-    <Collapse {isOpen} navbar expand="sm" on:update={handleUpdate}>
-      <Nav class="me-auto" navbar>
 
-
-        {#each navItems as {href, label}, i }
-          <NavItem>
-              <CustomNavLink {href} {label}/>
-
-
-          </NavItem>
-        {/each}
-
-      </Nav>
-    </Collapse>
-  </Navbar>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-sm">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">ConnCERT</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          {#each navItems as {href, label, faClass} }
+            <li class="nav-item">
+              <a class="nav-link rounded-3 mx-1 px-2 ${ $page.path.startsWith(href) ? 'active bg-primary text-primary' : ''}" style="--bs-bg-opacity: .2;" {href}>
+                <i class={faClass}></i>
+                {label}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    </div>
+  </nav>
