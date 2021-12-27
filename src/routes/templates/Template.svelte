57ad4756ec70525149c67c42997fdbfa246110c7
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-27 11:23:58 +0100

    styling + code cleanup

--- src/routes/templates/Template.svelte
+++ src/routes/templates/Template.svelte
@@ -1,8 +1,4 @@
 <script>
-    import { object_without_properties } from "svelte/internal";
-    // import selectedObject from "../index.svelte";
-
-    // es muss das on click event weiter geleitet werden. See svelte event propagation oder bubbeling kp wie das heisst
     export let template;
     export let selected;
 
@@ -16,9 +12,10 @@
 <div 
     on:mouseenter={toggleHover} 
     on:mouseleave={toggleHover} 
-    class="card mb-3 border-0 "
+    class="card mb-3  "
     class:hover-glow={isHovered && !selected}
     class:selected-glow={selected}
+    class:border-white={selected || isHovered}
     on:click 
     role="button"
 >
@@ -26,44 +23,21 @@
         <h5 class="card-title">{template.name}</h5>
         {#if template.matches.type}
             <span class="badge rounded-pill bg-dark">Type: {template.matches.type}</span>
-            
         {/if}
-
         {#if template.matches.level}
             <span class="badge rounded-pill bg-primary">Level: {template.matches.level}</span>
         {/if}
     </div>
-    
 </div>
    
-    <style>
-        .selected-glow{
-            box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 70%)
-        }
-        .hover-glow{
-            box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 40%)
-        }
-        .frame{
-        background: lightgrey;
-        border: 1px solid black;
-        }
-
-    </style>
-
- <!--   {#if template.selected == true}
-        <span class = "selected border border-2 rounded-pill fw-light fs-6"> Used </span>
-    {/if}
-
-    <div class = "template-body">
-        <span class = "badge badge-pill-info"> {template.selected} Used</span>
-    </div>
-</div>
-
 <style>
-
-    .frame{
-        background: lightgrey;
-        border: 1px solid black;
+    .selected-glow{
+        box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 70%)
     }
-</style> -->
+    .hover-glow{
+        box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 40%)
+    }
+
+</style>
+
 
--- src/routes/templates/[channel_id].svelte
+++ src/routes/templates/[channel_id].svelte
@@ -1,7 +1,5 @@
  <script context="module" lang="ts">
-    
-        
-        
+
         export async function load({fetch, page}){
             console.log("channel", page.params.channel_id);
             const url = 'http://localhost:3000/api/templates?' + new URLSearchParams({channel: page.params.channel_id})
@@ -19,53 +17,34 @@
         
 </script> 
 
-
-
 <script lang="ts">
     import Template from './Template.svelte';
-    import { Col, Container, Row } from 'sveltestrap';
-    import {
-    ButtonDropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle
-  } from 'sveltestrap';
 
+    export let templates;
+    console.log(templates);
 
-export let templates;
-console.log(templates);
-
-//   export let templates ({}){
-//       const defaultTemplate = Array.reduce()
-//   }
-//   export let templates;
-// const defaultTemplate = ? 
-// default template ist selected und matcht keinen type oder level (maybe use array.reduce)
-
-// const usedTemplates = (maybe use array.filter)
-
-const defaultTemplate = templates.filter(t=> t.used && t.matches.type === "" && t.matches.level === "")[0]
-// const usedTemplates = templates.filter(t => t.used && (t.matches.type != "" || t.matches.level != ""))
-let selectedTemplate = defaultTemplate
-const unusedTemplates = templates.filter(t => ! t.used)
-const usedTemplates = [...unusedTemplates]
+    const defaultTemplate = templates.filter(t=> t.used && t.matches.type === "" && t.matches.level === "")[0]
+    // const usedTemplates = templates.filter(t => t.used && (t.matches.type != "" || t.matches.level != ""))
+    let selectedTemplate = defaultTemplate
+    const unusedTemplates = templates.filter(t => ! t.used)
+    const usedTemplates = [...unusedTemplates]
 
 </script>
 
-<div class="wrapper">
+<div class="d-flex h-100">
     
-    <div class="w-50 px-3 pb-3 templates">
-        <h2 class="mt-3 text-dark">Default Template</h2>
+    <div class="w-50 px-5 pb-3 " style="overflow-y: scroll">
+        <h2 class="mt-3 text-dark ">Default Template</h2>
         <Template template={defaultTemplate} selected={selectedTemplate == defaultTemplate} on:click={()=>selectedTemplate = defaultTemplate}></Template>
 
 
-        <h2 class="mt-3 text-dark">Used Template</h2>
+        <h2 class="mt-5 text-dark ">Used Template</h2>
         {#each usedTemplates as template }
             <Template {template} selected={selectedTemplate === template} on:click={()=>selectedTemplate = template }></Template>
         {/each}
             
         
-        <h2 class="mt-3 text-dark">Unused Template</h2>
+        <h2 class="mt-5 text-dark ">Unused Template</h2>
         {#each unusedTemplates as template}
             <Template {template} selected={selectedTemplate === template} on:click={()=>selectedTemplate = template }></Template>
         {/each}
@@ -74,192 +53,22 @@ const usedTemplates = [...unusedTemplates]
     </div>
 
     
-    <div class="preview w-50">
-        <h2 class="text-right">Preview</h2>
-            <div id= "Preview">
+    <div class="w-50 position-relative">
+        <div class= "h-100 bg-dark text-white p-4" style="overflow-y: scroll">
+            {selectedTemplate.templateString}
+        </div>
+        <!-- <h2 class="text-right bg-primary shadow-sm">Preview</h2> -->
         
-            preview : {selectedTemplate.templateString}
-            </div>
-        <div id = "Button"> 
-            <div class="add text-center">
-                <form>
-                    <button class="btn btn-outline-success border-2 m-2">Used</button> 
-                    <button class="btn btn-outline-secondary border-2 m-2">Not used</button> 
-                    <button class="btn btn-outline-primary border-2 m-2">Edit</button> 
-                    <button class="btn btn-outline-danger border-2 m-2">Delete</button> 
-                </form>
-            </div>
+        <div class="position-absolute bottom-0 bg-light w-100   py-2 px-3"> 
+
+            {#if selectedTemplate.used}
+                <button class="btn btn-outline-secondary border-2 m-2">Do not use</button>
+            {:else}
+                <button class="btn btn-outline-secondary border-2 m-2">Use</button>
+            {/if}
+            <button class="btn btn-outline-primary border-2 m-2">Edit</button> 
+            <button class="btn btn-outline-danger border-2 m-2">Delete</button> 
+
         </div>
     </div>
 </div>
-
-
-<style>
-    .wrapper{
-        
-        height: 100%;
-        display: flex;
-        /* position: relative; */
-    }
-    .preview{
-        background: blue;
-        display: flex;
-        flex-direction: column;
-    }
-    .templates{
-        height: 100%;
-        overflow-y: scroll;
-    }
-
-
-    #Preview{ 
-        display: flex;
-        flex-grow: 1;
-        background: var(--channel-secondary);
-        border: 1px solid black;
-    }
-    .defaultTemplate{
-        color: var(--channel-on-secondary);
-        background-color: var(--channel-primary);
-    }
-    .usedTemplate{
-        margin-top: 100px;
-    }
-    .unusedTemplate{
-        margin-top: 100px;
-    }
-</style>
-
-
-
-
-<!-- 3. iterate over unused templates -->
-
-
-
-<!-- für preview -->
-<!-- <div>
-    {selecteTemplate.templateString}
-</div> -->
-
-
-
-<!-- 
-
-<div class="form-outline">
-    <input type="search" id="form1" class="form-control" placeholder="... search"
-     aria-label="Search" />
-</div>
-
-
-<p></p>
-
-<Container>
-
-</Container>
-
-    <div class="parent-container d-flex">
-
-        <div class="container">
-
-            <div class="row">
-                <ButtonDropdown >
-                    <DropdownToggle color="primary" caret>Template Level</DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem header>Header</DropdownItem>
-                      <DropdownItem>Some Action</DropdownItem>
-                      <DropdownItem disabled>Action (disabled)</DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>Foo Action</DropdownItem>
-                      <DropdownItem>Bar Action</DropdownItem>
-                      <DropdownItem>Quo Action</DropdownItem>
-                    </DropdownMenu>
-                </ButtonDropdown>
-
-                <p></p>
-                <h1 class="text-secondary">Templates</h1>
-                <div class="col rounded overflow-auto p-4 bg-secondary vh-100 m-3 ml-5 mr-5">
-                    <h4 class="text-light">Default Template</h4>
-                    <div class="container">
-                        <div class="text-center bg-light">
-                            <p>Twitter Default Template 3</p>
-                        </div>
-                        
-                    </div>
-                    <p></p>
-                    <h4 class="text-light">Used Templates</h4>
-                    <div class="container">
-                        <div class="text-center bg-light">
-                            <h6> Phishing Template</h6> 
-                            <p>type = "Phishing"</p>
-                        </div>
-                        <div class="text-center bg-light">
-                            <h6>Level 5 Template</h6>
-                            <p>level = 5</p>
-                        </div>
-                        <div class="text-center bg-light">
-                            <h6>Level 5 Virus</h6>
-                            <p>level = 5  &nbsp &nbsp &nbsp &nbsp  type = "Virus"</p>
-                        </div>
-
-                    </div>
-                    <p></p>
-                    <h4 class="text-light">Unused Templates</h4>
-                    <div class="container">
-                        <div class="text-center bg-light">
-                            <p>Template Name<br> Type <br> Level </p>
-                        </div>
-                        <div class="text-center bg-light">
-                            <p>Template Name<br> Type <br> Level </p>
-                        </div>
-                        <div class="text-center bg-light">
-                            <p>Template Name<br> Type <br> Level </p>
-                        </div>
-                    </div>                    
-                </div>
-                <div class="text-center">
-                    <button type="button" class="btn btn-primary">Primary</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="container ">
-            <div class="row">
-                <ButtonDropdown>
-                    <DropdownToggle color="primary" caret>Template Type</DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem header>Header</DropdownItem>
-                      <DropdownItem>Some Action</DropdownItem>
-                      <DropdownItem disabled>Action (disabled)</DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>Foo Action</DropdownItem>
-                      <DropdownItem>Bar Action</DropdownItem>
-                      <DropdownItem>Quo Action</DropdownItem>
-                    </DropdownMenu>
-                </ButtonDropdown>
-                <p></p>
-                <h1 class="text-secondary">Preview</h1>
-                <div class="col rounded bg-secondary vh-100 m-3 ml-5 mr-5" placeholder="placeholder">
-    
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <button type="button" class="btn btn-info">Edit</button>
-                        </div>
-                        <div class="col">
-                            <button type="button" class="btn btn-danger">Delete</button>
-                        </div>
-                        <div class="w-100"></div>
-                        <p></p>
-                        <div class="col">
-                            <button type="button" class="btn btn-success">Use</button>
-                        </div>
-                        <div class="col">
-                            <button type="button" class="btn btn-dark">Do not use</button>
-                        </div>
-                    </div>
-                  </div>
-            </div>
-        </div>
-    </div> -->
