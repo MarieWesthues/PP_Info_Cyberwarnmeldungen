5a58c7a6f2b867b5b433bb738540af68fa3aff97
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-28 12:58:02 +0100

    style different if selected as default

--- src/routes/templates/Template.svelte
+++ src/routes/templates/Template.svelte
@@ -1,6 +1,7 @@
 <script>
     export let template;
     export let selected;
+    export let isDefault;
 
     let isHovered = false;
     function toggleHover(){
@@ -19,13 +20,16 @@
     on:click 
     role="button"
 >
-    <div class="card-body">
-        <h5 class="card-title">{template.name}</h5>
+    <div class="card-body rounded  {isDefault ? "bg-primary text-light" : ""}" >
+        <h4 class="card-title">{template.name} </h4>
         {#if template.matches.type}
             <span class="badge rounded-pill bg-dark">Type: {template.matches.type}</span>
         {/if}
         {#if template.matches.level}
             <span class="badge rounded-pill bg-primary">Level: {template.matches.level}</span>
+        {/if}
+        {#if isDefault}
+                <span class="badge rounded-pill bg-light text-dark">Selected Default</span>
         {/if}
     </div>
 </div>
@@ -37,7 +41,6 @@
     .hover-glow{
         box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 40%)
     }
-
 </style>
 
 
