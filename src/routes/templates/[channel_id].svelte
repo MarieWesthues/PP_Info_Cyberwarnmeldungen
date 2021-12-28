dce189ae7f85480ccbea7e3c7404786a12ac0d87
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-28 12:58:27 +0100

    resolve conflicting templates with modal

--- src/routes/templates/[channel_id].svelte
+++ src/routes/templates/[channel_id].svelte
@@ -19,35 +19,66 @@
 
 <script lang="ts">
     import NewButton from '$lib/components/NewButton.svelte';
+import axios from 'axios';
+import { onMount } from 'svelte';
 import Template from './Template.svelte';
 
     export let templates;
     console.log(templates);
 
-    let defaultTemplate = templates.filter(t=> t.used && t.matches.type === "" && t.matches.level === "")[0]
-    let usedTemplates = templates.filter(t => t.used && t != defaultTemplate)
-    let selectedTemplate = defaultTemplate
+    // modal stuff
+    let modalRef;
+    let myModal;
+    onMount(async()=>{
+        myModal = new bootstrap.Modal(modalRef);
+    })
+
+    let collidingTemplate;
+
+    
+    let usedTemplates = templates.filter(t => t.used)
     let unusedTemplates = templates.filter(t => ! t.used)
+    $: defaultTemplate = usedTemplates.find(t=> t.matches.type === "" && t.matches.level === "")
+    let selectedTemplate = usedTemplates.find(t=> t.matches.type === "" && t.matches.level === "")
 
+    
 
-    function doNotUseTemplate(){
-        selectedTemplate.used = false;
-        unusedTemplates = [...unusedTemplates, selectedTemplate]
-        usedTemplates = usedTemplates.filter(t => t!= selectedTemplate);
+    function doNotUseTemplate(template){
+        template.used = false;
+        axios.put(`http://localhost:3000/api/templates/${template._id}`,{used: false})
+        unusedTemplates = [...unusedTemplates, template]
+        usedTemplates = usedTemplates.filter(t => t!= template);
     }
 
-    function useTemplate(){
-        const isUsable = ! usedTemplates.some(t => {
-            return (t.matches.level === selectedTemplate.matches.level) && (t.matches.type === selectedTemplate.matches.type)
+    function useTemplate(template){
+        let {type, level} = template.matches;
+
+        // find template collision
+        collidingTemplate = usedTemplates.find(t => {
+            return (t.matches.level === level) && (t.matches.type === type)
         })
-        if (isUsable) {
-            selectedTemplate.used = true;
-            unusedTemplates = unusedTemplates.filter(t => t!= selectedTemplate)
-            usedTemplates = [...usedTemplates, selectedTemplate]; 
+   
+        if (collidingTemplate) {
+            myModal.show()
+        }else{
+            template.used = true;
+            axios.put(`http://localhost:3000/api/templates/${template._id}`,{used: true})
+            unusedTemplates = unusedTemplates.filter(t => t!= template)
+            usedTemplates = [...usedTemplates, template]; 
         }
-        
-
     }
+
+    // will use the selected template and unuse the colliding template
+    function useTemplateInsteadOf(newTemplate, oldTemplate){
+        myModal.hide()
+        doNotUseTemplate(oldTemplate)
+
+        newTemplate.used = true;
+        axios.put(`http://localhost:3000/api/templates/${newTemplate._id}`,{used: true})
+        unusedTemplates = unusedTemplates.filter(t => t!= newTemplate)
+        usedTemplates = [...usedTemplates, newTemplate]; 
+    }
+
 
 </script>
 
@@ -55,19 +86,19 @@ import Template from './Template.svelte';
     
     <div class="w-50 position-relative " >
         <div class="px-5 pb-3 h-100" style="overflow-y: scroll">
-            <h2 class="mt-3 text-dark ">Default Template</h2>
+            <!-- <h2 class="mt-3 text-dark ">Default Template</h2>
             <Template template={defaultTemplate} selected={selectedTemplate == defaultTemplate} on:click={()=>selectedTemplate = defaultTemplate}></Template>
-
+ -->
 
             <h2 class="mt-5 text-dark ">Used Template</h2>
             {#each usedTemplates as template }
-                <Template {template} selected={selectedTemplate === template} on:click={()=>selectedTemplate = template }></Template>
+                <Template {template} selected={selectedTemplate === template} isDefault={defaultTemplate === template} on:click={()=>selectedTemplate = template }></Template>
             {/each}
                 
             
             <h2 class="mt-5 text-dark ">Unused Template</h2>
             {#each unusedTemplates as template}
-                <Template {template} selected={selectedTemplate === template} on:click={()=>selectedTemplate = template }></Template>
+                <Template {template} selected={selectedTemplate === template} isDefault={defaultTemplate === template} on:click={()=>selectedTemplate = template }></Template>
             {/each}
         </div>
         
@@ -92,9 +123,9 @@ import Template from './Template.svelte';
         <div class="position-absolute bottom-0 bg-light w-100   py-2 px-3"> 
 
             {#if selectedTemplate.used}
-                <button class="btn btn-outline-secondary border-2 m-2" on:click={doNotUseTemplate}>Do not use</button>
+                <button class="btn btn-outline-secondary border-2 m-2 " class:disabled={selectedTemplate === defaultTemplate} on:click={()=> doNotUseTemplate(selectedTemplate)}>Do not use</button>
             {:else}
-                <button class="btn btn-outline-secondary border-2 m-2" on:click={useTemplate} >Use</button>
+                <button class="btn btn-outline-secondary border-2 m-2" on:click={()=> useTemplate(selectedTemplate)} >Use</button>
             {/if}
             <a href="http://localhost:3000/templates/edit/{selectedTemplate._id}">
                 <button class="btn btn-outline-primary border-2 m-2">Edit</button>
@@ -104,3 +135,26 @@ import Template from './Template.svelte';
         </div>
     </div>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade" bind:this={modalRef} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          {#if collidingTemplate}
+          A template for type {selectedTemplate.matches.type} and level {selectedTemplate.matches.level} already exists. 
+          Do you want to use "{selectedTemplate.name}" instead of "{collidingTemplate.name}" ?
+          {/if}
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+          <button type="button" class="btn btn-primary" on:click={()=> useTemplateInsteadOf(selectedTemplate, collidingTemplate)}>Yes</button>
+        </div>
+      </div>
+    </div>
+  </div>
