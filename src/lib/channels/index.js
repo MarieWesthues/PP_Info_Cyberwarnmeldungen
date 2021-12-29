efd79256529885b0dcfc12c20ac614857702a9c9
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-29 13:10:14 +0100

    comment out send functionality

--- src/lib/channels/index.js
+++ src/lib/channels/index.js
@@ -4,18 +4,19 @@ import { sendTwitterAlert } from "./twitter";
 
 
 export function sendAlert(channelName, alertText, alertObject){
+    // Erstmal auskommentiert lassen pls
 
-    switch(channelName){
-        case 'Reddit':
-            sendRedditAlert(alertText, alertObject)
-            break;
-        case 'Twitter':
-            sendTwitterAlert(alertText, alertObject)
-            break;
-        case 'Email':
-            sendEmailAlert(alertText, alertObject)
-            break;
-        default: 
-            throw {name: "sendAlert failed", message: "The channelName you provided does not match any of the supported channels"}
-    }
+    // switch(channelName){
+    //     case 'Reddit':
+    //         sendRedditAlert(alertText, alertObject)
+    //         break;
+    //     case 'Twitter':
+    //         sendTwitterAlert(alertText, alertObject)
+    //         break;
+    //     case 'Email':
+    //         sendEmailAlert(alertText, alertObject)
+    //         break;
+    //     default: 
+    //         throw {name: "sendAlert failed", message: "The channelName you provided does not match any of the supported channels"}
+    // }
 }
