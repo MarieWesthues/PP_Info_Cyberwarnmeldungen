0b2b5e2567de3ae6d944c6a11fe19f74d29d0c14
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-29 15:10:35 +0100

    added additional fields + color indicator

--- src/routes/groups/Groups.svelte
+++ src/routes/groups/Groups.svelte
@@ -1,19 +1,44 @@
 <script>
     export let group;
-    export let anzahl = group.subscribers.length;
+    export let subscriberCount = group.subscribers.length;
 </script>
 
+<!-- padding start and overflow-hidden because of group color indicator -->
+<div class="card mx-auto mb-3 overflow-hidden ps-2" style="max-width: 50rem">
+  <!-- Group color indicator -->
+  <div class="h-100 position-absolute start-0 " style="width:.5rem; background:{group.color}"></div>
 
-<div class="card mx-auto mb-3" style="max-width: 50rem">
-    <h5 class="card-header fs-4">{group.name}</h5>
-      <div class="card-body">
-        <p class="card-text fw-light fs-6" ><span class="border border-2 rounded-pill">  Members = {anzahl}  </span></p>
-        <div class="buttons text-end">
-          <a href="/groups/{group._id}">
-              <button class="btn btn-outline-primary border-2">Edit</button>
+      <div class="flex-grow-1">
+        <div class="bg-white card-header d-flex align-items-center justify-content-between ">
+          <h5 class="fs-4 m-0">{group.name} </h5>
+          <a href="/groups/{group._id}" class="text-decoration-none float-right link-primary fs-5">
+              Review <i class="fas fa-chevron-right"></i>
           </a>
         </div>
-  </div> 
+        <div class="card-body px-4">
+          <div class="mb-3">
+            <i class="fas fa-users me-2"></i>{subscriberCount} { subscriberCount > 1 ? 'Subscribers' : 'Susbcriber'}
+    
+          </div>
+          <div class="overflow-hidden border-bottom border-1" style="max-height:4rem;">
+              {group.description}
+          </div>
+      </div>
+      </div>
+
+      
+
+
+
+
+
+
+ 
+ 
+      <!-- <div class="card-body">
+        <p class="card-text fw-light fs-6" ><span class="border border-2 rounded-pill">  Members = {anzahl}  </span></p>
+
+  </div>  -->
 </div>
 
 
