d3036d59ece0ac23b5fe19b20b2ba6e88983477b
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-12-29 14:30:59 +0100

    updated channel selection layout and style

--- src/routes/templates/Channel.svelte
+++ src/routes/templates/Channel.svelte
@@ -1,11 +1,35 @@
 <script>
-    import { object_without_properties } from "svelte/internal";
+    export let channel;
 
+    let isHovered = false;
 
-    export let obj;
+    function toggleHover(){
+        console.log("sheeeeees");
+        
+        isHovered = !isHovered;
+    }
 </script>
 
-<div class="">
-    Channel channel1
-    greeting = {obj.name}
-</div>
+
+
+<a on:mouseenter={toggleHover} on:mouseleave={toggleHover}  href="/templates/{channel.name}" class="text-decoration-none ">
+    <div class="card border-0 text-dark p-3 mb-4 transition-smooth { isHovered ? 'shadow-lg' : 'shadow-sm'}" >
+    
+        <div class="text-center p-4" style="height: 10rem">
+            <img src={channel.logo} alt="" class="img-fluid mh-100 transition-smooth" class:grow={isHovered}>
+        </div>
+            
+        <div class="card-body ">
+           <h3 class="card-title text-center transition-smooth" class:text-decoration-underline={isHovered}>{channel.name}</h3>
+        </div>
+    </div>
+</a>
+
+<style>
+    .transition-smooth{
+        transition: all .1s ease-in-out; 
+    }
+    .grow{
+        transform: scale(1.1);
+    }
+</style>
--- src/routes/templates/index.svelte
+++ src/routes/templates/index.svelte
@@ -1,7 +1,7 @@
 
 <script context="module" lang="ts">
     
-import Template from "./Template.svelte"; "./index.svelte";
+
     
     export async function load({fetch}){
         const url = '/api/channels';
@@ -17,75 +17,27 @@ import Template from "./Template.svelte"; "./index.svelte";
         }
     }
     
+
+
 </script>
 
 <script>
+import Channel from "./Channel.svelte";
+
     export let channels;
 </script>
 
-<!--<html lang="en">
-    <body> -->
-        
-        <div class="channel">
-            <h2>Templates</h2>
-            {#each channels as channel}
-                <!-- mit "--name:value" könnt ihr css variablen setzen.  -->
-                <a href="templates/{channel.name}" class = --channel:align-items-center style="text-decoration:none; --channel-primary:{channel.colors.primary}; --channel-primary:{channel.colors.primary}">
+<div class="container-sm ">
+    <h1 class="text-center m-5">Channels</h1>
 
-                        
-                    <div class="box justify-content-center grow">
-                        <!-- svelte-ignore a11y-missing-attribute -->
-                        <img class="rounded float-start p-2" width="80" height="80" src={channel.logo}>
-                        <h3 class= "text-light">{channel.name} </h3>
-                        
-                        
-                    </div>
-                 
-                </a>
-                
+        <div class="row">
+            {#each channels as channel}
+                <div class="col-md-4">
+                    <Channel {channel}/>
+                </div>
             {/each}
         </div>
+
+
     
-   <!-- </body>
-
-</html> -->
-
-
-<style>
-    .channel{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 1rem;     
-    }
-    .box{
-        /* mit dieser syntax könnt ihr css variablen benutzen */
-        background: var(--channel-primary);
-        
-        width: 20rem;
-        min-height: 5rem;
-        text-align: center;
-        margin: 15px;
-        border-radius: 10px;
-        position: relative;
-
-    }
-
-    .text-light{
-        margin: 0;
-        position: absolute;
-        top: 30%;
-        left: 48%;
-        -ms-transform: translateY(-50%, -50%);
-        transform: translateY(-50%, -50%);
-    }
-
-    .grow { 
-        transition: all .2s ease-in-out; 
-    }
-
-    .grow:hover { 
-        transform: scale(1.1);
-        filter: drop-shadow(10px 10px 10px rgb(167, 166, 166)); 
-    }
-</style>
+</div>
