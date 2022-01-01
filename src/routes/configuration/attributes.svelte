5de800361992e02d9725c09fb321a1d1da312f90
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2022-01-01 13:29:06 +0100

    finalized attribute table + input

--- src/routes/configuration/attributes.svelte
+++ src/routes/configuration/attributes.svelte
@@ -14,27 +14,90 @@
 </script>
 
 <script>
-    export let configuration;
+import Select from "$lib/components/Select.svelte";
+import axios from "axios";
+import { attr } from "svelte/internal";
 
+    export let configuration;
+    console.log(configuration);
+
+
+    function deleteAttribute(attr){
+        configuration.messageAttributes = configuration.messageAttributes.filter(a => a!= attr)
+        axios.put('http://localhost:3000/api/configuration', configuration.messageAttributes)
+    }
+
+    const attributeTypes = ['BOOLEAN', 'MULTISELECT', 'SELECT'] //TEXT noch
+    const defaultType = attributeTypes[0]
+    let newAttribute = {
+        key: "",
+        type: defaultType,
+        selectOptions: ""
+    }
+
+    function addNewAttribute(){
+        let {selectOptions, ...newAttributeObj} = newAttribute;
+        if (['MULTISELECT', 'SELECT'].includes(newAttributeObj.type)) {
+            newAttributeObj.selectOptions = newAttribute.selectOptions.split(",")
+        }
+        configuration.messageAttributes = [...configuration.messageAttributes, newAttributeObj]
+        
+        axios.put('http://localhost:3000/api/configuration', configuration.messageAttributes)
+        newAttribute = {key: "", type: defaultType, selectOptions: ""};
+    }
+
+    
 </script>
 
 <div class="container-sm">
     <h1 class="text-center p-4">Alert Attributes</h1>
 
-    <div class="mx-auto" style="max-width:50rem">
-        {#each configuration.messageAttributes as attribute}
-            <div class="mb-3">
-                <label class="form-label">CERT Name</label>
-                <input bind:value={configuration.name} type="text" class="form-control">
-            </div>
-        {/each}
-        
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Key</th>
+                <th scope="col">Type</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each configuration.messageAttributes as attribute, i}
+                <tr>
+                    <th scope="row">{i}</th>
+                    <td>{attribute.key}</td>
+                    <td>{attribute.type}</td>
+                    <td class="text-center">
+                        <span on:click={()=> deleteAttribute(attribute)}>
+                            <i role="button"  class="fas fa-trash-alt text-danger"></i>
+                        </span>
+                    </td>
+                </tr>
+            {/each}
+            <tr class="align-middle">
+                <th scope="row">
+                    <span class="badge bg-primary">New</span>
+                </th>
+                <td colspan="2">
+                    <div class="row my-3">
+                        <div class="col">
+                            <input class="form-control" bind:value={newAttribute.key}>
+                        </div>
+                        <div class="col">
+                            <Select bind:value={newAttribute.type} options={attributeTypes.map(a => ({value: a, label: a}))}/>
+                        </div>
+                    </div>
+                    <input bind:value={newAttribute.selectOptions} class="form-control" disabled={! ['SELECT', 'MULTISELECT'].includes(newAttribute.type)}  placeholder="option1, option2, option3, ...">
+ 
 
-        <div class="mb-3">
-            <label class="form-label">Reply email</label>
-            <input bind:value={configuration.replyEmail} type="text" class="form-control">
-        </div>
-    </div>
-
+                    
+                </td>
+   
+                <td class="text-center">
+                    <button class="btn btn-primary" on:click={addNewAttribute} ><i class="fas fa-plus"></i></button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
 </div>
