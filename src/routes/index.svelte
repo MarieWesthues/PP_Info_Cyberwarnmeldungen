cf9cdb9b042a2a9d350dc0aa1b24b133c3d44b02
JSimmou <jamal.simmou@stud.uni-due.de>
2022-01-03 19:54:31 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,14 @@
     <p style="font-size: 4rem; color:white"> Welcome to ConnCERT!</p>
     <p style="color:white; font-size: 18px;"> The Solution to send customized alerts easy and quickly. Reach numerous users from different target groups via various channels.</p>
 </div>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/N3NSdPh/fittosize-1200-0-c6e3608d9c2d43ffbc625a7dd7e4b0ce-logo-digicat-claim-rgb.png" alt="DigicatLogo" style="position: fixed; right: 3rem; top: 6rem;"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/XWYxLd5/Logo-claim-en-rgb-72dpi.png" alt="Logo_claim_en_rgb_72dpi" border="0" style="position: fixed; right: 3rem; bottom: 4rem;"></a>
+<a href="https://www.uni-due.de/digicat/">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src="https://www.uni-due.de/imperia/md/images/digicat/fittosize__1200_0_c6e3608d9c2d43ffbc625a7dd7e4b0ce_logo_digicat_claim_rgb.png" style="width:15rem; position: fixed; right: 3rem; top: 6rem;">
+</a>
+<a href="https://www.uni-due.de/">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src="https://www.uni-due.de/portal/4/images/UDE-logo-claim--en.svg" style="width:10rem; position: fixed; right: 3rem; bottom: 4rem;">
+</a>
 <p style="position: fixed; right: 3rem; bottom: 1rem;">A project by the University of Duisburg-Essen</p>
 
 <style>
