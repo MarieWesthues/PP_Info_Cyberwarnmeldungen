dbe668ca15be1774343aed1266e093df15e93f16
JSimmou <jamal.simmou@stud.uni-due.de>
2022-01-15 19:48:42 +0100

    Update index.svelte
    BG-Color Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <div class="sos">
-    <p style="font-size: 4rem; color:white"> Welcome to ConnCERT!</p>
+    <p style="font-size: 4rem; color:white"> Welcome to ConnCERT</p>
     <p style="color:white; font-size: 18px;"> The Solution to send customized alerts easy and quickly. Reach numerous users from different target groups via various channels.</p>
 </div>
 <a href="https://www.uni-due.de/digicat/">
@@ -22,7 +22,7 @@
         align-items: center;
         height: 100%;
         width: 100%;
-        background-image:linear-gradient(90deg, #0f45ff 0%, #8aa5f7 90%);
-
+        background: rgb(15,26,196);
+        background: linear-gradient(45deg, rgba(15,26,196,1) 0%, rgba(6,15,184,1) 9%, rgba(11,24,185,1) 17%, rgba(14,31,189,1) 24%, rgba(71,129,251,1) 62%, rgba(212,231,254,1) 86%, rgba(255,255,255,1) 100%);
 }
 </style> 
